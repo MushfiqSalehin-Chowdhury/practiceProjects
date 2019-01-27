d6c158c61da72cb789489d0a5f708c3e8ef3ef77
Mushfiq Salehin <mushfiq.chowdhury04@gmail.com>
2019-01-27 16:18:24 +0600

    Number shapes app

---
+++ app/src/main/java/com/example/mushfiq/practiceprojects/NumberShapes.java
@@ -0,0 +1,13 @@
+package com.example.mushfiq.practiceprojects;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NumberShapes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_shapes);
+    }
+}
