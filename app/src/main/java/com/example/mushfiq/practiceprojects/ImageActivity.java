f06c9c8b8c7d2f250404dd55a28a58afed02bf6a
Mushfiq Salehin <mushfiq.chowdhury04@gmail.com>
2019-01-28 16:47:08 +0600

    Image app update

--- app/src/main/java/com/example/mushfiq/practiceprojects/ImageActivity.java
+++ app/src/main/java/com/example/mushfiq/practiceprojects/ImageActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class ImageActivity extends AppCompatActivity {
+    int a=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +18,23 @@ public class ImageActivity extends AppCompatActivity {
     public void changePicture(View view) {
 
         ImageView iv= (ImageView) findViewById(R.id.imageView);
-        iv.setImageResource(R.drawable.car2);
+        ImageView iv2=(ImageView) findViewById(R.id.imageView2);
 
+        if (a==0){
+            iv.animate().alpha(0f).setDuration(1000);
+            iv2.animate().alpha(1f).setDuration(1000);
+            a=1;
+        }
+        else {
+            iv.animate().alpha(1f).setDuration(1000);
+            iv2.animate().alpha(0f).setDuration(1000);
+            a=0;
+        }
+
+       // iv.animate().alpha(1f).setDuration(2000);
+       // iv.setImageResource(R.drawable.car);
+        //iv.setImageResource(R.drawable.car2);
+        //iv.setImageResource(R.drawable.car2);
     }
 }
 
