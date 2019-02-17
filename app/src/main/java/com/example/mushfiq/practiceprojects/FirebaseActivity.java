0edf846e505005fd50a722741a4e24c97eb2186d
Mushfiq Salehin <mushfiq.chowdhury04@gmail.com>
2019-02-17 11:32:50 +0600

    Modified

--- app/src/main/java/com/example/mushfiq/practiceprojects/FirebaseActivity.java
+++ app/src/main/java/com/example/mushfiq/practiceprojects/FirebaseActivity.java
@@ -3,12 +3,17 @@ package com.example.mushfiq.practiceprojects;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.support.v7.widget.Toolbar;
 import com.google.android.gms.common.internal.safeparcel.SafeParcelable;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,50 +31,49 @@ public class FirebaseActivity extends AppCompatActivity{
     EditText name,email;
     TextView tv;
     int a=0;
-    List <String> users;
+    private List <User> usersList= new ArrayList<>();
+    private RecyclerView recyclerView;
+    private UserAdapter mAdapter;
+    RecyclerView.LayoutManager mlayoutManager;
     Random rand;
-    ArrayList<String> arrayList;
     DatabaseReference dbref,myref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase);
-        users = new ArrayList<>();
-        rand = new Random();
+        recyclerView=(RecyclerView) findViewById(R.id.recycle);
+        RecyclerView.LayoutManager mlayoutManager= new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mlayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         name = (EditText)findViewById(R.id.Name);
         email =(EditText)findViewById(R.id.Email);
-        tv= (TextView)findViewById(R.id.Details);
-        dbref = FirebaseDatabase.getInstance().getReference();
-        arrayList= new ArrayList<>();
-       // myref= dbref.child();
+        dbref = FirebaseDatabase.getInstance().getReference("Users");
+
     }
     public void SaveData(View view) {
-        Map<String, User> value = new HashMap<>();
-        value.put("User"+a++, new User(name.getText().toString(),email.getText().toString()));
-        dbref.push().setValue(value);
+        usersList.clear();
+        a=0;
+        User user=  new User(name.getText().toString(),email.getText().toString());
+        dbref.push().setValue(user);
         Toast.makeText(this,"Saved", Toast.LENGTH_SHORT).show();
-        tv.setVisibility(View.INVISIBLE);
-        //tv.setText(" ");
-       // arrayList.clear();
     }
     public void ShowData(View view) {
       dbref.orderByKey().addValueEventListener(new ValueEventListener() {
           @Override
           public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-              for (DataSnapshot child : dataSnapshot.getChildren()){
-                  String name = child.getValue().toString();
-                  arrayList.add(name);
-                 // tv.setText(name);
-                  User usr =  child.getValue(User.class);
+              for (DataSnapshot child : dataSnapshot.getChildren()) {
+                  User usr = new User("User"+a++,child.getValue().toString());
+                  usersList.add(usr);
+                  mAdapter=new UserAdapter(usersList);
+                  recyclerView.setAdapter(mAdapter);
+                  mAdapter.notifyDataSetChanged();
               }
-              Log.i("Users",arrayList.toString());
           }
           @Override
           public void onCancelled(@NonNull DatabaseError databaseError) {
           }
       });
-        tv.setVisibility(View.VISIBLE);
-        tv.setText(arrayList.toString());
-        arrayList.clear();
+
     }
 }
